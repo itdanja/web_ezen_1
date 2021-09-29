50a6ab540b793bf212369a8eb597170846424b77
itdanja <itdanja@kakao.com>
2021-09-29 11:23:05 +0900

    day02

--- java1/src/Day02/Day02_2.java
+++ java1/src/Day02/Day02_2.java
@@ -37,18 +37,94 @@ public class Day02_2 {
 		 * 	[논리]
 		 *  boolean	1비트			true , false
 		 *	[문자]
-		 *  char	2바이트		문자
+		 *  char	2바이트		문자[유니코드]
 		 *  [정수]
 		 *  byte	1바이트		+-127
 		 * 	short	2바이트		+-3만정도
 		 *  int		4바이트		+-20억정도
 		 *  long	8바이트		+-20억 이상 
 		 *  [실수]
-		 *  float	4바이트		
-		 *  double	8바이트 
+		 *  float	4바이트		소수점 8자리 표현
+		 *  double	8바이트 		소수점 18자리 표현
 		 */
-	
+		
+		// p.40 : byte [ -128 ~ +127 ]
+		byte var1 = -128; 	System.out.println( var1 );
+		byte var2 = -30;	System.out.println( var2 );
+		byte var3 = 30;		System.out.println( var3 );
+		// byte var4 = 128;	System.out.println( var4 ); // 오류
+		byte var4 = 'j';	System.out.println( var4 );
+		
+		// p.43 : char [ 한글자 ] : 유니코드 
+		char c1 = 'A'; 		System.out.println( c1 );
+		char c2 = 65;		System.out.println( c2 );
+		char c3 = '\u0041';	System.out.println( c3 );
+		
+		char c4 = '가';		System.out.println( c4 );
+		char c5 = 44032;	System.out.println( c5 );
+		char c6 = '\uac00';	System.out.println( c6 );
+		// 컴퓨터[0,1]는 문자를 어떻게 처리????????????
+			// 아스키코드 : 미국 [ 기계어 ----> 영어 -----> 기계어 ]
+			// 유니코드[java] : 전세계 모든 문자 표현 
+		
+		// p.44~45 : short[2바이트] , int[*기본 정수 자료형 / 4바이트] 타입
+		short num1 = 30000;			System.out.println( num1 );
+		//short num2 = 40000;		System.out.println( num2 );
+		int num2 = 10;				System.out.println( num2 );
+		int num3 = 012;				System.out.println( num3 ); // 8진수
+		int num4 = 0xA;				System.out.println( num4 ); // 16진수
+			// 진수 목적 : 컴퓨터가 표현할수 있는 단위 한계 해결
+			// 2진수 [ 0 , 1 ] : 기계어 
+			// 8진수 [ 0 ~ 7 ] :								[ 0 ]
+			// 10진수 [ 0 ~ 9 ] : 사람 						
+			// 16진수 [ 0 ~ 9 A B C D E F ] : 다양한 표현단위	[ 0x ]
+		
+		// p.46 : Long[ 8바이트 ] : 20억이상
+		Long num5 = 2000000000L; 	System.out.println( num5 );
+		
+		// p.47~48 : float[4바이트] , double[ *기본 실수 자료형 / 8바이트 ] 
+		double val = 3.14; 			System.out.println( val );
+		//float val2 = 3.14;		System.out.println( val2 );
+		float val2 = 3.14f;			System.out.println( val2 );
+		
+		double val3 = 0.1234567890123456789;	System.out.println( val3 );
+			// 소수점 18자리까지 표현 
+		float val4 = 0.1234567890123456789f;	System.out.println( val4 );
+			// 소수점 8자리까지 표현 
+		
+		// boolean : 1비트[0(false),1(true)]
+		boolean bol = true;			System.out.println( bol );
+		
+		// p.49~51 : 타입변환
+			// 1. 자동 타입변환 [ 크기순 : 작은상자에서 큰상자로 이동 가능 ]
+				// byte -> short(char) -> int -> long -> float -> double
+				// 작은상자에서 큰상자로 이동 가능 // 큰상자에서 작은상자로 이동 불가 
+		byte bytevalue = 10;
+		int intvalue = bytevalue; // 자동 형변환 [가능 ] byte -> int  
+		//byte bytevalue2 = intvalue;	// 불가  int -> byte 
+		
+			// 2. 강제 타입변환 [ 메모리손실 => 데이터 손실 ]
+				// 자료형 변수명 = (자료형)변수명;
+			// p.54
+		byte bytevalue2 = (byte)intvalue;
+		
+		
+		
+		
 	}
 	
 		
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
