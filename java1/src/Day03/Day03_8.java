9e7aade632f39ba91f2042c65553e88f95a25e6f
505-t <505-t@DESKTOP-JUVS72D>
2021-09-30 17:52:45 +0900

    day03

--- java1/src/Day03/Day03_8.java
+++ java1/src/Day03/Day03_8.java
@@ -45,3 +45,39 @@ public class Day03_8 {
 	}
 
 }
+	// 확인문제 
+		// 1. if , switch   // for , while , do~while
+		// 2. 2.
+		// 3. 
+			/*
+			 * 	int num = 0 ;
+			 * 	for( int i = 1 ; i<=100 ; i++ ){ if( i%3== 0 ) sum += i; }
+			 * 	System.out.println(" 3의 배수의 합 : "+ sum );
+			 */
+		// 4.----
+		// 5. 4x + 5y = 60
+			/*
+			 * 
+			 * 	for( int x = 1 ; x<=10 ; x++ ){ 
+			 * 		for( int y = 1 ; y<=10 ; y++) {
+			 * 			if(  (4*x)  +  (5*y ) == 60 ){ 
+			 * 				System.out.printf("(%d , %d) \n" , x , y ); 
+			 * 			}
+			 * 		}
+			 * 	}
+			 */
+		// 6.
+			/*
+			 * 	for( int i = 1 ; i<=5 ; i++ ) {
+			 * 
+			 * 		for( int s = 1 ; s<= i ; s++ ) { System.out.print("*"); }
+			 * 
+			 * 		System.out.println();
+			 * 
+			 * 	}
+			 */
+
+
+
+
+
