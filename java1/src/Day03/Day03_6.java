04137ea68c74753414cb59fc1a9d691620b0d57f
505-t <505-t@DESKTOP-JUVS72D>
2021-09-30 16:28:18 +0900

    day03

--- java1/src/Day03/Day03_6.java
+++ java1/src/Day03/Day03_6.java
@@ -7,7 +7,7 @@ public class Day03_6 {
 		// for( 초기값 ; 조건문 ; 증감식 ) { 실행문 }
 		
 		// 초기값
-		// while( 조건문 ){
+		// while( 조건문 ){  
 		//		실행문
 		//		증감식
 		// }
@@ -18,7 +18,6 @@ public class Day03_6 {
 			System.out.print( i+" "); // 3.실행문 
 			i++; // 4.증감식
 		}
-		
 	System.out.println("\n------------------------");
 		// for 차이 
 		for( int j = 1 ; j<=10 ; j++ ) {
@@ -36,9 +35,11 @@ public class Day03_6 {
 		int sum2 = 0 ;
 		for( int k = 1 ; k<=100 ; k++ ) { sum2+=k; }
 		System.out.println(" 1~100까지 누적합계 : "+sum2);
+	
+		// 무한루프 => while(true)
+		while(true) { System.out.println(" 계속실행중 "); }
+	
 	}
-	
-	
 	
 	
 	
