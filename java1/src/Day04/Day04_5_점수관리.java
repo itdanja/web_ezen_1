acba834b154291ce58cdf19110f67028b5ea1a30
505-t <505-t@DESKTOP-JUVS72D>
2021-10-01 15:02:29 +0900

    day04

--- java1/src/Day04/Day04_5_점수관리.java
+++ java1/src/Day04/Day04_5_점수관리.java
@@ -1,5 +1,7 @@
 package Day04;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Day04_5_점수관리 {
@@ -11,7 +13,7 @@ public class Day04_5_점수관리 {
 	public static void main(String[] args) {
 	
 		Scanner scanner = new Scanner(System.in);
-		int[] 학생점수목록 = null; // 배열선언후 공백넣기 
+		Integer[] 학생점수목록 = null; // 배열선언후 공백넣기 
 		
 		while(true) {
 			System.out.println("1.학생수 2.점수입력 3.분석 4.종료");
@@ -19,7 +21,7 @@ public class Day04_5_점수관리 {
 			if( ch == 1 ) {
 				System.out.print(" 학생 수 입력[배열길이] : "); int size = scanner.nextInt();
 				// 배열에 메모리 할당 
-				학생점수목록 = new int[size]; // 입력받은 수 만큼 메모리 할당 
+				학생점수목록 = new Integer[size]; // 입력받은 수 만큼 메모리 할당 
 				System.out.println(size +"만큼 학생목록 생성");
 			}
 			if( ch == 2 ) {
@@ -30,6 +32,35 @@ public class Day04_5_점수관리 {
 				}
 			}
 			if( ch == 3 ) {
+				// 1. 내림차순 
+//				for(int i = 0 ; i<학생점수목록.length ; i++ ) {
+//					for( int j = i+1 ; j<학생점수목록.length ; j++ ) {
+//						if( 학생점수목록[i] < 학생점수목록[j] ) {
+//							// 스왑 [ 교체 ] 
+//							int temp = 학생점수목록[j];
+//							학생점수목록[j] = 학생점수목록[i];
+//							학생점수목록[i] = temp;
+//						}
+//					}
+//				}
+				// 정렬 순서도 
+				// i = 0 		j = 1 		j = 1 2 3 
+				// i = 1 		j = 2 		j = 2 3 
+				// i = 2		j = 3		j = 3 
+				// i = 3		j = 4		실행x
+				
+				// 2. 클래스 이용한 정렬  [ 배열 관련 클래스 = Arrays ] 
+				Arrays.sort( 학생점수목록 ); // 오름차순 / 오름차순 반대는 내림차순 
+				Arrays.sort( 학생점수목록, Collections.reverseOrder() );
+						// Collections.reverseOrder() : 순서 뒤집기 [ 기본 자료형은 불가 // 객체만 가능 ]
+				
+				// 2. 출력 
+				int sum = 0 ;
+				for( int i = 0 ; i<학생점수목록.length ; i++ ) {
+					System.out.println( (i+1) + "등 점수 : " + 학생점수목록[i]);
+					sum += 학생점수목록[i];
+				}
+				System.out.println(" 모든 학생들의 점수 평균 : " +  sum / 학생점수목록.length );
 				
 			}
 			if( ch == 4) break;
