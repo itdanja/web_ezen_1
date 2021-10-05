2cb3eb0b4707891319dcaf93fdf959b9078f08ef
505-t <505-t@DESKTOP-JUVS72D>
2021-10-05 17:58:59 +0900

    day05

---
+++ java1/src/Day05/Member.java
@@ -0,0 +1,30 @@
+package Day05;
+
+public class Member {
+	
+	// 1. 필드
+	String 아이디;
+	String 비밀번호;
+	String 이름;
+	int 연락처;
+	
+	// 2. 생성자
+	
+	// 3. 메소드 
+	public void 회원가입() {
+		// 실행코드
+	}
+	public void 로그인() {
+		// 실행코드
+	}
+	public void 회원탈퇴() {
+		// 실행코드
+	}
+	public void 회원수정() {
+		
+	}
+	
+	
+	
+
+}
