652138425171144bc6d9cd481a72179403918cfd
505-t <505-t@DESKTOP-JUVS72D>
2021-10-06 13:58:41 +0900

    day06

--- java1/src/Day06/Day06_4.java
+++ java1/src/Day06/Day06_4.java
@@ -44,6 +44,26 @@ public class Day06_4 {
 			 * 		this.id = id;
 			 * 	}
 			 */
+		// 15 :
+			/*
+			 * 	// 1. 로그인 메소드 [ 인수 : id , password 반환 : true ]
+				boolean login( String id , String password ) {
+					
+					if( id.equals("hong") && password.equals("1234") ) {
+						return true; // 동일할경우 true 리턴 
+					}else {
+						return false; // 동일하지 않을경우 fasle 리턴 
+					}
+				}
+				// 2. 로그아웃 메소드 [ 인수 : id , 반환 x ] 
+				void logout( String id  ) {
+					System.out.println( id +" 님 로그아웃 되었습니다 ");
+				}
+			 */
+		// 16~19 : 생략 
+	
+		// 20
+	
 	
 	
 }
