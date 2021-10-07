bf7ba101a1dac44a0cdf5932db33ec61779c688d
505-t <505-t@DESKTOP-JUVS72D>
2021-10-07 15:51:17 +0900

    day07

--- java1/src/Day07/Book.java
+++ java1/src/Day07/Book.java
@@ -54,14 +54,49 @@ public class Book {
 	
 		// 3. 도서목록 메소드 
 	public void book_list() {
+		System.out.println("------------------도서 목록 페이지 ----------------");
+		System.out.println("도서코드\t도서명\t대여여부\t대여회원");
+		
+		for( int i = 0 ; i< BookApplication.books.length ; i++ ) {
+			if( BookApplication.books[i] == null ) return;
+			
+			Book book = BookApplication.books[i];
+			System.out.println( book.getB_ISBN()+"\t"+book.getB_name()
+								+"\t"+book.getB_rental()+"\t"+book.m_id );
+		}
 		return;
 	}
-		// 4. 도서대여 메소드 
+		// 4. 도서대여 메소드 [ 도서코드를 입력받아 대여여부가 TRUE이면 대여하기 ]  
 	public boolean book_rental() {
-		return true;
+		System.out.println("------------------도서 대여 페이지 ----------------");
+		System.out.println(" 대여할 도서코드 : ");	String b_ISBN = BookApplication.scanner.next();
+			// ISBN 존재여부 체크 
+		boolean check = bookcheck(b_ISBN);
+		if( check ) {
+			
+			for( int i = 0 ; i<BookApplication.books.length ;i++ ) {
+				if( BookApplication.books[i] != null && 
+						BookApplication.books[i].getB_ISBN().equals(b_ISBN)) { // 동일한 도서가 존재하면
+					// 대여여부 확인 
+					if( BookApplication.books[i].getB_rental() ) {
+						System.out.println(" [[ 알림 ]] : 현재 도서 대여 합니다. 대여성공!!! ");
+						// 1. 대여성공시 대여여부 true => false
+						BookApplication.books[i].setB_rental(false);
+						// 2. 대여성공시 대여회원에 현재 로그인된 아이디 대입 
+						return true;
+					}
+					else { System.out.println(" [[ 알림 ]] : 현재 대여중인 도서 입니다. 대여불가!!!"); return false; }
+				}
+			}	
+		}else {
+			System.out.println("[[알림]] : 해당 도서코드가 존재하지 않습니다");
+			return false;
+		}
+		return false;
 	}
 		// 5. 도서반납 메소드 
 	public boolean book_return() {
+	
 		return true;
 	}
 	
