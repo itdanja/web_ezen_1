4145a50690134c238247ba29828bb1afb31b0a4e
505-t <505-t@DESKTOP-JUVS72D>
2021-10-07 15:31:11 +0900

    day07

--- java1/src/Day07/Book.java
+++ java1/src/Day07/Book.java
@@ -5,12 +5,12 @@ public class Book {
 	// 1. 필드 [ ISBN , 도서명 , 대여여부 , 대여한 회원아이디 등 ]
 	private String b_ISBN;
 	private String b_name;
-	private String b_rental;
+	private boolean b_rental;
 	private String m_id;
 	
 	// 2. 생성자
 		// 1. 도서등록 [ 모든 필드 ]
-	public Book(String b_ISBN, String b_name, String b_rental, String m_id) {
+	public Book(String b_ISBN, String b_name, boolean b_rental, String m_id) {
 		this.b_ISBN = b_ISBN;
 		this.b_name = b_name;
 		this.b_rental = b_rental;
@@ -22,8 +22,36 @@ public class Book {
 	// 3. 메소드
 		// 2. 도서등록 메소드 
 	public boolean book_add() {
-		return true;
+		// 1. 입력받기 
+		System.out.println("------------------도서 등록 페이지 ----------------");
+		System.out.println(" 도서코드 : ");	String b_ISBN = BookApplication.scanner.next();
+			// 도서코드 중복체크 
+			boolean check = bookcheck(b_ISBN);
+			if( check ) {
+				System.out.println(" [[ 알림 ]] : 동일한 도서코드가 존재합니다 ");
+				return false;
+			}
+		System.out.println(" 도서명 : ");		String b_name = BookApplication.scanner.next();
+		// 2. 객체 생성		// 도서대여여부 초기값 => true 		// 대여한 회원 초기값 => null
+		Book book = new Book(b_ISBN, b_name, true , null);
+		// 3. 객체 배열저장 
+		for( int i = 0 ; i< BookApplication.books.length ; i++ ) {
+			if( BookApplication.books[i] == null ) { 
+				BookApplication.books[i] = book ; return true; }
+		}
+		return false;
 	}
+	// 6. ISBN 체크 메소드 [ 체크할 대상을 인수로 받아서 배열내 존재하면 true 아니면 false ]
+	public boolean bookcheck( String check_isbn ) {
+		for( int i = 0 ; i< BookApplication.books.length ; i++ ) {
+			if( BookApplication.books[i] != null && 
+					BookApplication.books[i].getB_ISBN().equals(check_isbn) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 		// 3. 도서목록 메소드 
 	public void book_list() {
 		return;
@@ -36,10 +64,7 @@ public class Book {
 	public boolean book_return() {
 		return true;
 	}
-		// 6. ISBN 체크 메소드 [ 체크할 대상을 인수로 받아서 배열내 존재하면 true 아니면 false ]
-	public boolean bookcheck( String check_isbn ) {
-		return true;
-	}
+	
 		// 1. 필드가 private 사용시 => get ,set 메소드 
 	public String getB_ISBN() {
 		return b_ISBN;
@@ -53,10 +78,10 @@ public class Book {
 	public void setB_name(String b_name) {
 		this.b_name = b_name;
 	}
-	public String getB_rental() {
+	public boolean getB_rental() {
 		return b_rental;
 	}
-	public void setB_rental(String b_rental) {
+	public void setB_rental(boolean b_rental) {
 		this.b_rental = b_rental;
 	}
 	public String getM_id() {
--- java1/src/Day07/BookApplication.java
+++ java1/src/Day07/BookApplication.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class BookApplication { // 실행 클래스 
 	
+//	------------------- 회원제 도서관 프로그램 ---------------------
+//	// [조건1] : 객체지향 [ 회원 클래스 , 도서 클래스  ]
+//		설계 : 필드 , 생성자 , 메소드
+//	// [조건2] : 객체는 배열에 저장
+//	// [조건3] : 회원만 프로그램 이용
+//		[ 로그인전 ] : 로그인 , 회원가입 , 아이디/비밀번호 찾기   
+//		[ 로그인시 ] : 1.도서목록 ,2. 대여 , 3.반납  , 4.로그아웃
+//	// [조건4] : 관리자[ ID:admin ] 만 도서 등록 가능 
+//		[ 관리자 로그인시 ] : 1.도서등록 ,2.도서목록 3. 로그아웃 
+	
 	// 0. 배열 
 	public static Member[] members = new Member[100];
 	public static Book[] books = new Book[100];
@@ -78,7 +88,7 @@ public class BookApplication { // 실행 클래스
 			System.out.println(" ---------- Admin Menu ----------");
 			System.out.println(" 1.도서등록 | 2.도서목록 | 3.로그아웃 ");
 			System.out.print("선택 : ");	int ch = scanner.nextInt();
-			if( ch == 1 ) { book.book_add(); }
+			if( ch == 1 ) { book.book_add();}
 			if( ch == 2 ) { book.book_list(); } 
 			if( ch == 3 ) { System.out.println(" [[ 알림 ]] : 로그아웃 되었습니다 "); return; }
 		}
