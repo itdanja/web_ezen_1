f5259cc2da0ba5ec7b36109742fbedd0e17e256f
505-t <505-t@DESKTOP-JUVS72D>
2021-10-19 15:48:46 +0900

    day14

--- java1/src/Day14/Day14_4_회원제_List.java
+++ java1/src/Day14/Day14_4_회원제_List.java
@@ -1,5 +1,6 @@
 package Day14;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,6 +14,8 @@ public class Day14_4_회원제_List {
 		Scanner scanner = new Scanner(System.in);
 		// 0.설정 [ 리스트 선언 : User 클래스만 담을수 있는 클래스 ( User클래스의 하위도 가능) ] 
 		ArrayList<User> users = new ArrayList<>();
+		// 0.설정 [ 파일 읽어오기 : 파일에 있는 회원들을 리스트에 저장 ]
+		
 		
 		// 1. 메뉴 
 		while(true) {
@@ -39,6 +42,26 @@ public class Day14_4_회원제_List {
 				// 3. 리스트에 저장 [ 만약에 중복값이 없으면 ]
 				if( idcheck ) {users.add( user ); System.out.println(" [[ 회원가입 성공 ]]");}
 				// 4. 파일처리 
+					// 1. 파일출력 클래스
+							// new FileOutputStream("파일경로/파일명.확장자" );  새로쓰기 
+							// new FileOutputStream("파일경로/파일명.확장자" , true );  이어쓰기 
+					try {
+						
+						FileOutputStream fileOutputStream = 
+							new FileOutputStream("C:/Users/505-t/git/web_ezen_1/java1/src/Day14/userlist.txt" , true );
+						
+						// 2. 필드간 구분 [ , ]  // 회원간 구분 [ \n ]
+						String outstring = user.getId()+","+user.getPassword()+","+user.getName()+"\n";
+						
+						// 3. String ---> bytes 내보내기 [ write( 바이트열 ) ]
+						fileOutputStream.write( outstring.getBytes() );
+					
+						System.out.println(" [[ 파일 처리 성공 ]]");
+				
+					}
+					catch (Exception e) {
+						System.out.println(" [[ 파일 처리 실패 ]]");
+					}
 				
 			}
 			else if( ch == 2  ) {
