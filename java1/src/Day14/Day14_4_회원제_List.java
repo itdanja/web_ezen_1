5378ac86223edb15827c0c4f8f02f6145507ecad
505-t <505-t@DESKTOP-JUVS72D>
2021-10-19 16:11:14 +0900

    day14

--- java1/src/Day14/Day14_4_회원제_List.java
+++ java1/src/Day14/Day14_4_회원제_List.java
@@ -1,5 +1,6 @@
 package Day14;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -15,7 +16,39 @@ public class Day14_4_회원제_List {
 		// 0.설정 [ 리스트 선언 : User 클래스만 담을수 있는 클래스 ( User클래스의 하위도 가능) ] 
 		ArrayList<User> users = new ArrayList<>();
 		// 0.설정 [ 파일 읽어오기 : 파일에 있는 회원들을 리스트에 저장 ]
-		
+			
+		// 1. 파일입력 클래스 
+		try {
+			FileInputStream fileInputStream = 
+				new FileInputStream("C:/Users/505-t/git/web_ezen_1/java1/src/Day14/userlist.txt");
+			
+			// 1. 파일내 모든 글자 가져오기  [ 스트림 단위 : 바이트 ]
+				// 1. 바이트배열 선언
+				byte[] bytes = new byte[1024]; // 1bit -> 8bit -> 1byte -> 1024byte -> 1kbyte
+						// 영문,공백,특수문자 1바이트 // 한글 2바이트
+				// 2. 바이트 읽어오기 
+				fileInputStream.read( bytes );
+				// 3. 바이트 -> 문자열 변화
+				String inString = new String(bytes);
+				
+			// 2. 회원 구분하기 
+				String[] usernum =  inString.split("\n");
+								// split( 기준 ) 분리 => 배열 
+			// 3. 필드 구분하기 
+				
+				for( String temp : usernum ) {
+					String[] userdata =  temp.split(",");
+					if( userdata.length == 1 ) break;
+						// * 마지막 회원(공백회원) 제외 
+					// 4. 객체 생성 
+					 User user = new User( userdata[0] , userdata[1] , userdata[2] );
+					// 5. 리스트 담기
+					users.add( user);
+				}
+		}
+		catch (Exception e) {
+			System.out.println(" [[ 파일 불러오기 실패 ]]");
+		}
 		
 		// 1. 메뉴 
 		while(true) {
@@ -40,29 +73,28 @@ public class Day14_4_회원제_List {
 						}
 					}
 				// 3. 리스트에 저장 [ 만약에 중복값이 없으면 ]
-				if( idcheck ) {users.add( user ); System.out.println(" [[ 회원가입 성공 ]]");}
-				// 4. 파일처리 
-					// 1. 파일출력 클래스
-							// new FileOutputStream("파일경로/파일명.확장자" );  새로쓰기 
-							// new FileOutputStream("파일경로/파일명.확장자" , true );  이어쓰기 
-					try {
+				if( idcheck ) {users.add( user ); System.out.println(" [[ 회원가입 성공 ]]");
+					// 4. 파일처리 
+						// 1. 파일출력 클래스
+								// new FileOutputStream("파일경로/파일명.확장자" );  새로쓰기 
+								// new FileOutputStream("파일경로/파일명.확장자" , true );  이어쓰기 
+						try {	
+							FileOutputStream fileOutputStream = 
+								new FileOutputStream("C:/Users/505-t/git/web_ezen_1/java1/src/Day14/userlist.txt" , true );
+							
+							// 2. 필드간 구분 [ , ]  // 회원간 구분 [ \n ]
+							String outstring = user.getId()+","+user.getPassword()+","+user.getName()+"\n";
+							
+							// 3. String ---> bytes 내보내기 [ write( 바이트열 ) ]
+							fileOutputStream.write( outstring.getBytes() );
 						
-						FileOutputStream fileOutputStream = 
-							new FileOutputStream("C:/Users/505-t/git/web_ezen_1/java1/src/Day14/userlist.txt" , true );
-						
-						// 2. 필드간 구분 [ , ]  // 회원간 구분 [ \n ]
-						String outstring = user.getId()+","+user.getPassword()+","+user.getName()+"\n";
-						
-						// 3. String ---> bytes 내보내기 [ write( 바이트열 ) ]
-						fileOutputStream.write( outstring.getBytes() );
+							System.out.println(" [[ 파일 처리 성공 ]]");
 					
-						System.out.println(" [[ 파일 처리 성공 ]]");
-				
-					}
-					catch (Exception e) {
-						System.out.println(" [[ 파일 처리 실패 ]]");
-					}
-				
+						}
+						catch (Exception e) {
+							System.out.println(" [[ 파일 처리 실패 ]]");
+						}
+				}
 			}
 			else if( ch == 2  ) {
 				// 1. 입력받기 
