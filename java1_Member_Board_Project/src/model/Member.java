e72aaaafb5657f6d6e55cc091e16e5291786d4af
505-t <505-t@DESKTOP-JUVS72D>
2021-10-21 10:20:20 +0900

    day16 design

---
+++ java1_Member_Board_Project/src/model/Member.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Member {
+
+	// 1. 필드 
+	private String id;
+		// private : 
+	private String password;
+	private String name;
+	private String email;
+	private int point;
+	
+	// 2. 생성자 : 생성자를 사용하는 이유?? 
+	public Member() {
+	
+	}
+	public Member(String id, String password, String name, String email, int point) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.point = point;
+	}
+	// 3. 메소드 [ get , set 사용하는 이유 ?? 
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+}
---
+++ java1_Member_Board_Project/src/view/Application.java
@@ -0,0 +1,29 @@
+package view;
+
+import java.util.Scanner;
+
+public class Application {
+
+	// 0.설정 [ main 밖에 만드는 이유?? :  ] 
+	public static Scanner scanner = new Scanner(System.in);
+		// public :
+		// static : 
+	
+	public static void main(String[] args) {
+		mainmenu(); // 메인 메뉴 메소드 호출
+	}
+	
+	// 1. 메인 메뉴 메소드 
+	public static void mainmenu( ) {
+		// void : 
+	}
+	// 2. 회원 메뉴 메소드
+	public static void membermenu( String id ) {
+								// 인수 : 
+		
+	}
+	// 3. 게시판 메뉴 메소드 
+	public static void boardmenu( String id ) {
+				
+	}
+}
