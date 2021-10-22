33b5aed4bfe18380565650ff6f17c7dd5c41702d
505-t <505-t@DESKTOP-JUVS72D>
2021-10-22 14:00:28 +0900

    day17 - board , boardcontroller

---
+++ java1_Member_Board_Project/src/controller/BoardController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.Board;
+
+public class BoardController {
+
+	// 1. 필드
+	public static ArrayList<Board> boards = new ArrayList<>();
+		// static 없을경우에 해당 필드 호출시 [ BoardController 객체 생성 ]
+		// static 있을경우 해당 필드 바로호출 가능 
+	
+	// 2. 메소드 
+	// 게시물 등록 메소드 
+	public static boolean add( Board board ) {
+		
+		return true; // 등록 성공시 
+	}
+	// 게시물 상세페이지[조회] 메소드 
+	public static Board detail( int index ) {
+		
+		Board board = null;
+		return board; // 검색한 게시물 반환
+	}
+	// 게시물 삭제 메소드 
+	public static boolean delete( int index ) {
+		return true;
+	}
+	// 게시물 수정 메소드 
+	public static boolean update( int index , String title , String contents ) {
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ java1_Member_Board_Project/src/model/Board.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Board {
+
+	// 1. 필드 
+	private String title;
+	private String contents;
+	private String writer;
+	private Date date;
+	private int view;
+	// 하나의 게시물의 여러개 댓글 저장하기 위한 
+	private ArrayList<Reply> replylist;  // 댓글 리스트 메모리할당
+	
+	// 2. 생성자 
+	public Board() { // 빈생성자 : 1.메소드호출용시 2. 빈 객체 생성후 추후에 필드 값 대입 
+		
+	}
+	// 게시물 등록시 사용되는 생성자 
+	public Board(String title, String contents, String writer) {
+		
+		this.title = title;
+		this.contents = contents;
+		this.writer = writer;
+		
+		Date date = new Date(); // 현재시간 클래스
+		this.date = date;
+		
+		this.view = 1;
+		this.replylist = new ArrayList<>(); 
+		
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getView() {
+		return view;
+	}
+	public void setView(int view) {
+		this.view = view;
+	}
+	public ArrayList<Reply> getReplylist() {
+		return replylist;
+	}
+	public void setReplylist(ArrayList<Reply> replylist) {
+		this.replylist = replylist;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
