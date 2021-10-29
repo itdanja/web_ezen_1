035507ebdc08aed22c33ef045d8985f07d896021
505-t <505-t@DESKTOP-JUVS72D>
2021-10-29 11:19:02 +0900

    day22

--- java2/src/controller/SignupController.java
+++ java2/src/controller/SignupController.java
@@ -3,6 +3,8 @@ package controller;
 import domain.Member;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -69,7 +71,6 @@ public class SignupController {
 	    	if( txtemail.getText().length() <5 && !txtemail.getText().contains("@") ) {
 	    		lblconfirm.setText("이메일 형식으로 입력해주세요."); return; 
 	    	}
-	    
     	// 2. 중복체크 
     	
     	// 3. 객체화 
@@ -78,15 +79,22 @@ public class SignupController {
     	
     	// 4. 파일 혹은 DB 처리
     	
-    	// 5.  
     	lblconfirm.setText("가입해주셔서 감사합니다");
+    	// 5. 메시지창 띄우고 페이지 전환
+    		Alert alert = new Alert( AlertType.INFORMATION ); // 1. 메시지 객체 생성 
+    		alert.setContentText("  회원가입 성공  "); // 2. 메시지 내용 
+    		alert.setHeaderText(" Nike community 가입을 축하합니다. * 포인트 1000 지급 *"); // 3. 메시지 제목 
+    		alert.setTitle("알림");
+    		alert.showAndWait(); // 4. 메시지를 띄우고 버튼 입력시까지 대기 
+    		LoginController.getinstance().loadpage("login"); // 로그인fxml 파일로 이동 
+   
     }
 
     @FXML
     void back(MouseEvent event) {
     		// !! 새로운 객체 선언시에는 새로운 메모리
     	// LoginController loginController = new LoginController();
-    		
+    	
     		// 동일한 메모리 사용하기 위해서 this 키워드를 사용한 객체 호출 
     	LoginController.getinstance().loadpage("login");
     }
