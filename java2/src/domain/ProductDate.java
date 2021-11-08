ec7286b9be1d6814727930a0a1b8246110097950
itdanja <itdanja@kakao.com>
2021-11-08 14:07:05 +0900

    day28

---
+++ java2/src/domain/ProductDate.java
@@ -0,0 +1,31 @@
+package domain;
+
+public class ProductDate {
+	private String date; // xÃà
+	private int count;	// yÃà 
+	
+	public ProductDate() {
+	}
+
+	public ProductDate(String date, int count) {
+		this.date = date;
+		this.count = count;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+}
